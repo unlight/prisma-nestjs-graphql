67e85400e4b64c28c6c77bec1790a0d4700ccd80
Roman_Vasilev <s91630277@gmail.com>
2021-02-22 04:04:41 +0400

    remove dup all clean

--- src/mutate-filters/remove-duplicate-types.ts
+++ src/mutate-filters/remove-duplicate-types.ts
@@ -1,7 +1,7 @@
 import { isEqual } from 'lodash';
 
 import { InputType } from '../types';
-import { RemoveDuplicate, generateHash } from '../utils';
+import { generateHash, RemoveDuplicate } from '../utils';
 
 export function removeDuplicateTypes(
     inputTypes: InputType[],
@@ -11,68 +11,23 @@ export function removeDuplicateTypes(
     for (const inputType of inputTypes) {
         const hash = generateHash(inputType.fields);
         duplicates.set(hash, (duplicates.get(hash) ?? new Set()).add(inputType.name));
-        // for (const otherType of inputTypes) {
-        //     if (otherType.name === inputType.name) {
-        //         continue;
-        //     }
-        //     if (isEqual(inputType.fields, otherType.fields)) {
-        //         duplicates.set(hash, otherType.name)
-        //         if (removeDuplicate === RemoveDuplicate.All) {
-        //             duplicates[otherType.name] = inputType.name;
-        //         } else if (removeDuplicate === RemoveDuplicate.Group) {
-        //             const groupName = getGroupName(otherType);
-        //             if (groupName) {
-        //                 duplicates[otherType.name] = groupName;
-        //             }
-        //         }
-        //     }
-        // }
     }
-    const toRemove = [...duplicates.values()]
-        .filter(s => s.size > 1)
-        .map(set => {
-            const main = getNameFromAll([...set]);
-            return [...set].filter(x => x !== main);
-        })
-        .flat();
-
-    console.log('toRemove', toRemove);
-    // console.log('duplicates', duplicates);
+    const toRemove = new Set(
+        [...duplicates.values()]
+            .filter(s => s.size > 1)
+            .map(set => {
+                const main = getNameFromAll([...set]);
+                return [...set].filter(x => x !== main);
+            })
+            .flat(),
+    );
 
     return (inputType: InputType) => {
-        if (toRemove.includes(inputType.name)) {
+        if (toRemove.has(inputType.name)) {
             return false;
         }
         return inputType;
     };
-
-    // // Create groups
-    // const inputTypeByName = mapKeys(inputTypes, 'name');
-    // const replacements: Record<string, string> = {};
-    // for (const inputType of inputTypes) {
-    //     const group = groupFactories.find(({ match }) => match(inputType));
-    //     if (group) {
-    //         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    //         const model = group.model(group.match(inputType)!);
-    //         const name = group.name(model);
-    //         // Compare fields
-    //         const primaryType = inputTypeByName[name];
-    //         // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    //         if (primaryType && isEqual(inputType.fields, primaryType.fields)) {
-    //             replacements[inputType.name] = primaryType.name;
-    //         }
-    //     }
-    // }
-    // console.log('replacements', replacements);
-
-    // return (inputType: InputType) => {
-    //     const newName = replacements[inputType.name];
-    //     if (newName && newName !== inputType.name) {
-    //         // TODO: Check do we need rename?
-    //         return false;
-    //     }
-    //     return inputType;
-    // };
 }
 
 function getNameFromAll(names: string[]) {
