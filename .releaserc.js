cf35fa546e523cd0338c35a71e84f3889ae7cff1
Roman_Vasilev <s91630277@gmail.com>
2021-03-13 01:47:53 +0400

    ci: Updated semantic release config

--- .releaserc.js
+++ .releaserc.js
@@ -1,7 +1,17 @@
 module.exports = {
     plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                preset: 'conventionalcommits',
+            },
+        ],
+        [
+            '@semantic-release/release-notes-generator',
+            {
+                preset: 'conventionalcommits',
+            },
+        ],
         '@semantic-release/changelog',
         [
             '@semantic-release/npm',
