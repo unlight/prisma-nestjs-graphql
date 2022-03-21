b582e0518931e627e73e8411a4ccf10ac37b04c0
Roman_Vasilev <s91630277@gmail.com>
2022-03-21 20:13:26 +0400

    build: Config commitlint

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     rules: {
         'subject-case': [2, 'always', ['sentence-case']],
         'scope-case': [2, 'always', ['lower-case', 'upper-case']],
-        'body-max-line-length': [2, 'always', 200],
+        'body-max-line-length': [1, 'never'],
+        'footer-max-line-length': [1, 'never'],
     },
 };
