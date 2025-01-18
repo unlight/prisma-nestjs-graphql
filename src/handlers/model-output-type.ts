c69a10bc5abc5266c6e4ece8a68253717add881d
Roman <s91630277@gmail.com>
2025-01-18 12:38:36 +0400

    fix: Cannot find model by name
    close: #228

--- src/handlers/model-output-type.ts
+++ src/handlers/model-output-type.ts
@@ -11,7 +11,7 @@ import {
 } from 'ts-morph';
 
 import { createComment } from '../helpers/create-comment';
-import { isCreateManyReturn } from '../helpers/create-many-return';
+import { isManyAndReturnOutputType as isManyAndReturnOutputType } from '../helpers/is-many-and-return';
 import { getGraphqlImport } from '../helpers/get-graphql-import';
 import { getOutputTypeName } from '../helpers/get-output-type-name';
 import { getPropertyType } from '../helpers/get-property-type';
@@ -30,7 +30,7 @@ export function modelOutputType(outputType: OutputType, args: EventArguments) {
   const { getSourceFile, models, config, modelFields, fieldSettings, eventEmitter } =
     args;
 
-  if (isCreateManyReturn(outputType.name)) return;
+  if (isManyAndReturnOutputType(outputType.name)) return;
 
   const model = models.get(outputType.name);
   ok(model, `Cannot find model by name ${outputType.name}`);
