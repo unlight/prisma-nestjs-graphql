8e596cbc0636f1f2f26e8d538abf0168ebce9d46
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2026-01-29 12:28:39 -0500

    chore: cleaning up @generated content

--- @generated/profile/profile-avg-aggregate.input.ts
+++
@@ -1,9 +0,0 @@
-import { Field } from '@nestjs/graphql';
-import { InputType } from '@nestjs/graphql';
-
-@InputType()
-export class ProfileAvgAggregateInput {
-
-    @Field(() => Boolean, {deprecationReason:'Use new name instead',nullable:true})
-    id?: true;
-}
--- @generated/profile/profile-sum-aggregate.input.ts
+++
@@ -1,9 +0,0 @@
-import { Field } from '@nestjs/graphql';
-import { InputType } from '@nestjs/graphql';
-
-@InputType()
-export class ProfileSumAggregateInput {
-
-    @Field(() => Boolean, {deprecationReason:'Use new name instead',nullable:true})
-    id?: true;
-}
